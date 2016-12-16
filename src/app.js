252bd9204a4c2f663ec0bb462b1a25fdad6365c9
m.faraponov <mikefaraponov@gmail.com>
2016-12-16 14:26:28 +0200

    Removed express application

--- src/app.js
+++ src/app.js
@@ -1,54 +1,10 @@
-const express = require('express');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const compression = require('compression');
-const REDIS = require('./constants').REDIS;
-const api = require('./api');
+/* eslint new-cap: [0, {capIsNewExceptions: ["S"]}] */
+const server = require('http').Server();
+const io = require('socket.io')(server);
 const geoApi = require('./geo-api');
 
-const app = express();
-/* eslint new-cap: [0, {capIsNewExceptions: ["S"]}] */
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
 const nsp = io.of('/api');
 
-if(process.env.NODE_ENV === 'production' && process.env.CLUSTER_MODE) {
-  io.adapter(require('socket.io-redis')({
-    host: REDIS.HOST,
-    port: REDIS.PORT,
-  }));
-}
-
-app.set('trust proxy', 1);
-
-app.use(logger('dev'))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended: false}))
-  .use(cookieParser());
-
-if(process.env.NODE_ENV === 'production') {
-  app.disable('x-powered-by');
-  app.use(helmet())
-    .use(compression());
-}
-
-app.use('/api', api);
-
 nsp.on('connection', geoApi(nsp));
-
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500)
-    .send(res.locals.message);
-});
 
 module.exports = server;
